e6ef553855696ec1528442a53bfab296887f963f
mohamed3500 <mohamed3500@gmail.com>
2025-02-22 16:09:49 +0200

    fifth commit

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import style from "./checkout.module.css";
+import style from "./Checkout.module.css";
 import { useFormik } from "formik";
 
 import { UserContext } from "../../Contexts/UserContext";
